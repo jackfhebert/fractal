b7edf404e79b5c4334e099286507c4a64333e3bc
Jack Hebert <jhebert@gmail.com>
2014-01-12 20:18:46 -0800

    basics work

--- fractal.go
+++ fractal.go
@@ -4,24 +4,25 @@ import (
        "image"
        "image/color"
        "image/png"
+       "math/cmplx"
+       "log"
        "os"
-       "png"
 )
 
 var (
   numSteps int = 1024
 )
 
-func RenderFractoal() image.Image {
-  minX, maxX, minY, maxY = -512, 512, -512, 512
-  image := image.NewNRGBA(Rect(minX, minY, maxX, maxY))
+func RenderFractal() image.Image {
+  minX, maxX, minY, maxY := -512, 512, -512, 512
+  image := image.NewNRGBA(image.Rect(minX, minY, maxX, maxY))
 
-  xStepSize := float64(maxX - minX) / numSteps
-  yStepSize := float64(maxY - minY) / numSteps
+  xStepSize := (maxX - minX) / numSteps
+  yStepSize := (maxY - minY) / numSteps
 
   for currX := minX; currX < maxX; currX += xStepSize {
     for currY := minY; currY < maxY; currY += yStepSize {
-      currColor := color.NRGBA(int(currX) % 128, int(currY) % 128, 0, 0)
+      currColor := GetColor(currX, currY)
       image.SetNRGBA(int(currX), int(currY), currColor)
     }
   }
@@ -29,9 +30,20 @@ func RenderFractoal() image.Image {
   return image
 }
 
+func GetColor(x, y int) color.NRGBA {
+  z := complex(float64(x) / 256.0, float64(y) / 256)
+  c := z
+  steps := 0
+  for ; steps < 127 && cmplx.Abs(z) < 2; steps += 1 {
+    z = z * z + c
+  }
+
+ return color.NRGBA{uint8(steps), 127, 127, 127}
+}
+
 func SaveImage(image image.Image) {
   // Try to open the file, die if it doesn't work.
-  file, err = os.Create("fractal.png")
+  file, err := os.Create("fractal.png")
   if err != nil {
     log.Fatal(err)
   }
